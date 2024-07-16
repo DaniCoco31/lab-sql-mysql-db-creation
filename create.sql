b141a8bbdc9ca6d76944f63f15689ba79825f440
Danicoco31 <danitrujillo20@gmail.com>
2024-07-16 11:30:07 +0200

    delete added

--- create.sql
+++ create.sql
@@ -1,27 +1,24 @@
 CREATE DATABASE IF NOT EXISTS lab_mysql;
 USE lab_mysql;
-
 -- Drop tables if they exist
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS salespersons;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS cars;
 
-
 -- Create Cars table
 CREATE TABLE cars (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    vin VARCHAR(17) UNIQUE NOT NULL,
+    car_id INT AUTO_INCREMENT PRIMARY KEY,
+    vin VARCHAR(17) NOT NULL,
     manufacturer VARCHAR(50) NOT NULL,
     model VARCHAR(50) NOT NULL,
-    manufactured_year YEAR NOT NULL,
+    car_year YEAR NOT NULL,
     color VARCHAR(20) NOT NULL
 );
 
 -- Create Customers table
 CREATE TABLE customers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cust_id INT UNIQUE NOT NULL,
+    customer_id  INT AUTO_INCREMENT PRIMARY KEY,
     cust_name VARCHAR(100) NOT NULL,
     cust_phone VARCHAR(15) NOT NULL,
     cust_email VARCHAR(100),
@@ -34,22 +31,19 @@ CREATE TABLE customers (
 
 -- Create Salespersons table
 CREATE TABLE salespersons (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    staff_id INT UNIQUE NOT NULL,
+    staff_id INT AUTO_INCREMENT PRIMARY KEY,
     salesperson_name VARCHAR(100) NOT NULL,
     store VARCHAR(100) NOT NULL
 );
 
 -- Create Invoices table
 CREATE TABLE invoices (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    invoice_number VARCHAR(50) UNIQUE NOT NULL,
+    invoice_number INT AUTO_INCREMENT PRIMARY KEY,
     inv_date DATE NOT NULL,
     car_id INT NOT NULL,
     customer_id INT NOT NULL,
     salesperson_id INT NOT NULL,
-    FOREIGN KEY (car_id) REFERENCES cars(id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id),
-    FOREIGN KEY (salesperson_id) REFERENCES salespersons(id)
+    FOREIGN KEY (car_id) REFERENCES cars(car_id),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (salesperson_id) REFERENCES salespersons(staff_id)
 );
-
--- seeding.sql
+++ seeding.sql
@@ -1,14 +1,15 @@
 -- Insert sample data into Cars table
-INSERT INTO cars (vin, manufacturer, model, manufactured_year, color)
+INSERT INTO cars (vin, manufacturer, model, car_year, color)
 VALUES
     ('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
     ('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
     ('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
     ('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
-    ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray');
+    ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
+    ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
 
 -- Insert sample data into Customers table
-INSERT INTO customers (cust_id, cust_name, cust_phone, cust_email, cust_address, cust_city, cust_state, cust_country, cust_zipcode)
+INSERT INTO customers (customer_id, cust_name, cust_phone, cust_email, cust_address, cust_city, cust_state, cust_country, cust_zipcode)
 VALUES
     (10001, 'Pablo Picasso', '+34636176382', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
     (20001, 'Abraham Lincoln', '+13059077086', NULL, '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
